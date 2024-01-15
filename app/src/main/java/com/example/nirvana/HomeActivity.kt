6f2c6b54516f64e1022460a35311ee7ae63f6cb9
aanckr <a.annacker@student.xu-university.de>
2024-01-15 18:35:59 +0100

    Added: Toolbar for navigation + profile picture

--- app/src/main/java/com/example/nirvana/HomeActivity.kt
+++ app/src/main/java/com/example/nirvana/HomeActivity.kt
@@ -2,10 +2,56 @@ package com.example.nirvana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_collections -> {
+                    // for now:
+                    Toast.makeText(applicationContext, "Clicked Collections", Toast.LENGTH_SHORT).show()
+                }
+                R.id.nav_contact_us -> {
+                    // for now:
+                    Toast.makeText(applicationContext, "Clicked Contact Us", Toast.LENGTH_SHORT).show()
+                }
+                R.id.nav_log_out -> {
+                    // for now:
+                    Toast.makeText(applicationContext, "Clicked Log Out", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       if (toggle.onOptionsItemSelected(item)) {
+           return true
+       }
+        return super.onOptionsItemSelected(item)
     }
 }
