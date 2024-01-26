c397cfb88efb878fd375b9c841cd946b589a9365
aanckr <a.annacker@student.xu-university.de>
2024-01-26 14:18:09 +0100

    Added: Backend for log in page

--- app/src/main/java/com/example/nirvana/LoginActivity.kt
+++ app/src/main/java/com/example/nirvana/LoginActivity.kt
@@ -1,11 +1,53 @@
 package com.example.nirvana
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.nirvana.databinding.ActivityLoginBinding
+import com.example.nirvana.databinding.ActivitySignUpBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.continueBtn.setOnClickListener{
+            val username :String = binding.username.text.toString()
+            val password :String = binding.password.text.toString()
+
+            if(username.isEmpty()){
+                Toast.makeText(this, "Please enter your username", Toast.LENGTH_SHORT).show()
+            } else if(password.isEmpty()){
+                Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show()
+            }else{
+                readData(username, password)
+            }
+        }
+    }
+
+    private fun readData(username: String, password: String){
+        database = FirebaseDatabase.getInstance().getReference("Users")
+        database.child(username).get().addOnSuccessListener{
+            if(it.exists()){
+                val user = it.getValue(User::class.java)
+                if(user?.username == username && user?.password == password){
+                    Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, HomeActivity::class.java))
+                }else{
+                    Toast.makeText(this, "Incorrect data", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "User does not exist", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener{
+            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
